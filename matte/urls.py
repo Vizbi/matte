29a7aa9b38255e5435d5e4bf35534fc8ad2fa760
Yogesh Kumar <yk.301189@gmail.com>
2017-05-18 17:20:00 +0530

    Refactor

--- matte/urls.py
+++ matte/urls.py
@@ -1,5 +1,16 @@
 from django.conf.urls import url
-from .views import test_route_url
+from django.shortcuts import render_to_response
+
+from .models import Storyboard
+from .services import Board
+
+
+def test_route_url(request, url):
+    storyboard = Storyboard.objects.get(url=url)
+    board = Board(storyboard)
+    board = {'board': board.get_chart()}
+    return render_to_response('matte/test.html', board)
+
 
 urlpatterns = [
     url(r'^(?P<url>[-\w]+)/$', test_route_url, name='test_route_url'),
--- matte/views.py
+++ matte/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render_to_response
 
-from .models import Storyboard
-from .services import Board
-import matte.viz
-
-
-def test_route_url(request, url):
-    storyboard = Storyboard.objects.get(url=url)
-    board = Board(storyboard)
-    board = {'board': board.get_chart()}
-    return render_to_response('matte/test.html', board)
+#create your views here.
